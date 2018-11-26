b5922feb508999b1feb9288d6d291c830999266c
miro-jk <miro.jk.dev@gmail.com>
2018-11-26 02:14:53 +0100

    no install prompts

--- raspbian/install.sh
+++ raspbian/install.sh
@@ -1,3 +1,3 @@
-sudo apt-get install xterm npm
+sudo apt-get -y install xterm npm
 mkdir -p ~/.config/autostart
 cp ~/alarm_clock/raspbian/lxterm-autostart.desktop ~/.config/autostart/lxterm-autostart.desktop
